6abd1ea9914b64840d0981165a505b4153150b97
Goran Radmanovic <goranradmanovic@gmail.com>
2020-02-25 13:09:15 +0100

    New Loader partial

---
+++ components/partials/Loader.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  v-overlay(:value='loading' opacity='0.9')
+    v-progress-circular(indeterminate size='64')
+</template>
+
+<script>
+// import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Loader',
+  data: () => ({
+    loading: false
+  }),
+  methods: {
+    start () {
+      this.loading = true
+    },
+    finish () {
+      this.loading = false
+    }
+  }
+}
+</script>
